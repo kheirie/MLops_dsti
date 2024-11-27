5a8c07cfbacba60e0aa77eed39127d9a13f8ed6c
Kheirie <kaderi.kheirie@gmail.com>
2024-11-27 23:38:38 +0100

    updated path

--- docker_testing/test/test_docker.py
+++ docker_testing/test/test_docker.py
@@ -1,6 +1,11 @@
 import sys
+import os
 # Always run from unit_testing_best_practice/test
 sys.path += ['../src'] 
+src_path = os.path.abspath(os.path.join("docker_testing", "src"))
+
+if src_path not in sys.path:
+    sys.path.append(src_path)
 
 from mul import *
 
